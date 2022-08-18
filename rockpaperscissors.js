76990b8023393a0a201b734551503856ec6eb2ef
MGP1992 <mathewgparkinson@gmail.com>
2022-08-18 10:55:00 +0100

    Add UI to play game

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,40 +2,16 @@ function getComputerChoice() {
     let choiceArray = ['Rock', 'Paper', 'Scissors'];
     let randomNumber = getRandomInt(3)
     let computerChoice = choiceArray[randomNumber];
-    console.log(`Computer selected ${computerChoice}`);
+    computerSelection.textContent = `Computer selected ${computerChoice}`;
     return computerChoice;
 }
 
-function getPlayerChoice() {
-    let keepGoing = true;
-    while (keepGoing) {
-        let playerChoice = prompt('Please enter Rock, Paper or Scissors');
-        let correctCasePlayerChoice = correctInputCase(playerChoice);
-        if (checkPlayerChoice(correctCasePlayerChoice)){
-            keepGoing = false;
-            console.log(`Player selected ${correctCasePlayerChoice}`);
-            return correctCasePlayerChoice;
-            }else alert('Please enter Rock, Paper or Scissors'); 
-    }
-}
-    
-function checkPlayerChoice(playerChoice) {
-    if (playerChoice === 'Rock' || playerChoice === 'Paper' || playerChoice === 'Scissors'){
-        return true;
-    }else return false;
-}
+  
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function correctInputCase(playerSelection) {
-    let lowerCase = playerSelection.toLowerCase();
-    let lowerCaseSlice = lowerCase.slice(1)
-    let upperCase = playerSelection.toUpperCase();
-    let upperCaseSlice = upperCase.slice(0,1);
-    return `${upperCaseSlice}${lowerCaseSlice}`;
-}
 
 function compareChoices(playerChoice, computerChoice) {
 
@@ -66,47 +42,64 @@ function compareChoices(playerChoice, computerChoice) {
     }
 }
 
-function playRound() {
+function handleGameCounter(){
+    gameCounter++;
+    gamesPlayed.textContent = `Games played = ${gameCounter}`
+}
 
-    let playerChoice = getPlayerChoice();
+function playRound(choice) {
+    let playerChoice = choice;
+    playerSelection.textContent = `You have selected: ${choice}`;
     let computerChoice = getComputerChoice();
     let gameOutcome = (compareChoices(playerChoice, computerChoice));
-    return gameOutcome;
-}
-
-function playGame() {
-    let gameCounter = 1
-    let playerWinCount = 0;
-    let computerWinCount = 0;
-    let drawCounter = 0;
-    while(gameCounter < 6) {
-        let gameOutcome = playRound();
-        if (gameOutcome.includes(`You win!`)) {
-            alert(gameOutcome);
-            playerWinCount ++;
-            gameCounter++;
-        }else if (gameOutcome.includes(`You lose!`)){
-            alert(gameOutcome);
-            computerWinCount ++;
-            gameCounter++;
-        }else 
-            {alert(gameOutcome);
-                drawCounter++;
-                gameCounter++;}
-
+    roundResult.textContent = `${gameOutcome}`
+    if (gameOutcome.includes(`You win!`)) {
+        playerWinCount ++;
+        playerScore.textContent = `Current player score = ${playerWinCount}`;
+        handleGameCounter();
+    }else if (gameOutcome.includes(`You lose!`)){
+        computerWinCount ++;
+        computerScore.textContent = `Current computer score = ${computerWinCount}`;
+        handleGameCounter();
+    }else{ 
+        drawCounter++;
+        gamesDrawn.textContent = `Games drawn = ${drawCounter}`;
+        handleGameCounter();
     }
-
-    alert(`After 5 games the final scores are:
-    Player: ${playerWinCount}
-    Computer: ${computerWinCount}
-    Games drawn: ${drawCounter}`)
+    
 }
 
-playGame();
+
+    // alert(`After 5 games the final scores are:
+    // Player: ${playerWinCount}
+    // Computer: ${computerWinCount}
+    // Games drawn: ${drawCounter}`)
+
+let gameCounter = 0;
+let playerWinCount = 0;
+let computerWinCount = 0;
+let drawCounter = 0;
 
 
+let rockButton = document.querySelector('#rock');
+let paperButton = document.querySelector('#paper');
+let scissorsButton = document.querySelector('#scissors');
+
+let playerScore = document.querySelector('#playerscore');
+let computerScore = document.querySelector('#computerscore');
+let gamesDrawn = document.querySelector('#gamesdrawn');
+let gamesPlayed = document.querySelector('#gamesplayed');
+
+let playerSelection = document.querySelector('#playerselection');
+let computerSelection = document.querySelector('#computerselection');
+let roundResult = document.querySelector('#roundresult');
 
 
+rockButton.addEventListener('click', () => {playRound('Rock')});
+paperButton.addEventListener('click', () => {playRound('Paper')});
+scissorsButton.addEventListener('click',() => {playRound('Scissors')});
+
+ 
 
 
 
